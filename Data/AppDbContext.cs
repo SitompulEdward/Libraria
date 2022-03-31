843e6effaca5acba7ad845a5906d3afec86823a9
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2022-03-31 09:43:38 +0700

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Libraria.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libraria.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Roles> Rolse { get; set; }
+        public virtual DbSet<Admin> Admin { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Pemasok> Pemasok { get; set; }
+        public virtual DbSet<Buku> Buku { get; set; }
+        public virtual DbSet<Peminjaman> Peminjaman { get; set; }
+        public virtual DbSet<Pengembalian> Pengembalian { get; set; }
+    }
+}
